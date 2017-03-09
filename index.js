e437950811f08bf29bb55c75f279111714f5a8ab
Thomas Arrow <thomasarrow@gmail.com>
2017-03-09 13:53:31 +0000

    simple hard coded stress

---
+++ index.js
@@ -0,0 +1,12 @@
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+    host: 'localhost:9200'
+})
+
+var makeQuery = function () {
+    client.ping({}, function (err) {
+	if (err) throw err
+	else makeQuery()
+	}
+}
