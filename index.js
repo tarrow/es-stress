1f6798613d6cdf659abbe1473810a66cd7ced30f
Thomas Arrow <thomasarrow@gmail.com>
2017-03-09 16:59:44 +0000

    use keepalive like karoo

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var elasticsearch = require('elasticsearch');
+var elasticsearch = require('elasticsearch')
+var AgentKeepAlive = require('agentkeepalive')
 
 var client = new elasticsearch.Client({
     host: 'localhost:9200'
+    maxSockets: 20,
+    maxRetries: 50,
+    createNodeAgent: function (connection, config) {
+      return new AgentKeepAlive(connection.makeAgentConfig(config))
+    }
 })
 
 var makeQuery = function () {
